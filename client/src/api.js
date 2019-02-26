4d8d092920ef1e01cc8c3db5dd1b97d6a435032f
JonesTPG <joonas.ryynanen1@gmail.com>
2019-02-26 22:00:50 +0200

    https

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:5000');
+const socket = openSocket('https://localhost:5000');
 
 function subscribeToTimer(cb) {
   socket.on('timer', timestamp => cb(null, timestamp));
