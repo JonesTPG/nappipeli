50c8ccb39250e6305a7f532e7b93b190950c6195
JonesTPG <joonas.ryynanen1@gmail.com>
2019-03-02 14:15:10 +0200

    heroku url.

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import io from 'socket.io-client'
 
 //initialize a websocket connection to the server.
-var socket = io.connect('http://localhost:5000'); //local
-//var socket = io.connect('https://thawing-wildwood-25973.herokuapp.com'); //heroku
+//var socket = io.connect('http://localhost:5000'); //local
+var socket = io.connect('https://thawing-wildwood-25973.herokuapp.com'); //heroku
 
 //passing the socket object down as a prop so we don't have to establish multiple websocket connections.
 ReactDOM.render(<App socket={socket}/>, document.getElementById('root'));
